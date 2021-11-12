7da139f6789b41af61547ffe676c1476cd4f17f6
Lucas Henrique <lucas.hrv@outlook.com>
2021-11-12 09:09:56 -0300

    commit

---
+++ javascript/js/main.js
@@ -0,0 +1,21 @@
+//comentario
+/* Introdução */
+console.log("Ola mundo");
+alert("Ola mundo");
+
+var num = 0;
+var str = 'ola mundo';
+let boolean = true;
+
+while(num < num2){
+    console.log(num += 1);
+}
+
+if(num < 10){
+    console.log("ola mundo", num);
+
+}
+
+console.log(num, num2, num3);
+
+console.log(document.getElementById('nome'))
